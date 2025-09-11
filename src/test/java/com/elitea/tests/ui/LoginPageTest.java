dcbee6f905e7f46241b74745d2844b0683502779
emil-bagirov-epam <emilbagirovepam@gmail.com>
2025-09-11 12:59:21 +0300

    [SCRUM-87] Add missing Page import for WaitForSelectorOptions

--- src/test/java/com/elitea/tests/ui/LoginPageTest.java
+++ src/test/java/com/elitea/tests/ui/LoginPageTest.java
@@ -1,6 +1,7 @@
 package com.elitea.tests.ui;
 
 import com.elitea.ui.pages.LoginPage;
+import com.microsoft.playwright.Page;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
